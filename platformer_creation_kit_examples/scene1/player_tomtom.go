af74f8b4e06c9814d28b840fab20b53a740e5c6a
jasperdj <jjasperdejong@gmail.com>
2018-12-26 21:14:52 +0100

    inital commit

---
+++ platformer_creation_kit_examples/scene1/player_tomtom.go
@@ -0,0 +1,357 @@
+components {
+  id: "camera_target"
+  component: "/platformer_creation_kit/framework/scripts/camera_target.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "input"
+  component: "/platformer_creation_kit_examples/scene1/player_tomtom_input.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "inventory"
+  component: "/platformer_creation_kit/framework/scripts/inventory.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "health"
+  component: "/platformer_creation_kit/framework/scripts/health.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "movement"
+  component: "/platformer_creation_kit_examples/scene1/player_tomtom_movement.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "move_acceleration"
+    value: "5000.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "sprint_speed_modifier"
+    value: "3.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "max_speed"
+    value: "450.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "gravity"
+    value: "-8500.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "jump_takeoff_speed"
+    value: "3000.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "knockback_force"
+    value: "1250.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/platformer_creation_kit/framework/atlases/tomtom.atlas\"\n"
+  "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 120.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "swing_factory"
+  type: "factory"
+  data: "prototype: \"/platformer_creation_kit/framework/gameobjects/swing.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"player\"\n"
+  "mask: \"obstacle\"\n"
+  "mask: \"spikes\"\n"
+  "mask: \"pickup\"\n"
+  "mask: \"enemy\"\n"
+  "mask: \"speech\"\n"
+  "mask: \"one_sided\"\n"
+  "mask: \"ladder\"\n"
+  "mask: \"ladder_end\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -0.68\n"
+  "      y: 119.624\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 183.8565\n"
+  "  data: 121.811\n"
+  "  data: 10.2\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "jump_sound"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/jump.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "footstep2"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/footstep2.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ladder_step2"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/ladder_step2.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "swing"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/swing00.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "footstep1"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/footstep1.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "footstep3"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/footstep3.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ladder_step1"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/ladder_step1.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ladder_step3"
+  type: "sound"
+  data: "sound: \"/platformer_creation_kit/framework/sounds/ladder_step3.ogg\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
